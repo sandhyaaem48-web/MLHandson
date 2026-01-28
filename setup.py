2d2c13d8ed2d9f3c21dc8961a52a2c11e0269339
sandhyaaem48-web <sandhyaaem48@gmail.com>
2026-01-27 20:03:25 -0500

    Basic Project structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path):
+    requirements = []
+
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='MLHANDSON',
+    version='0.1.0',
+    author='Sandhya',
+    author_email='sandhyaaem48@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
